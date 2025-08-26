71e70cd50f217351845bf02df3abd399f58a343c
kipp <batchelork3@cardiff.ac.ukgit config --global user.email batchelork3@cardiff.ac.uk>
2025-08-26 22:03:54 +0100

    started work on js version, much easier. Have added a successful checkbox in the form and a currently useless reset button

--- app.py
+++ app.py
@@ -9,38 +9,38 @@ app = Flask(__name__)
 def index():
     
     action_type = None
-    x_start_coordinate = "0.00"
-    y_start_coordinate = "0.00"
-    x_end_coordinate = "0.0"
-    y_end_coordinate = "0.00"
+    x_start_coordinate = None
+    y_start_coordinate = None
+    x_end_coordinate = None
+    y_end_coordinate = None
 
     if request.method == "POST":
-        # Get selected action type from the form
+        #Get selected action type from the form
         action_type = request.form.get("action_type")
         x_start_coordinate = request.form.get("x_start")
         y_start_coordinate = request.form.get("y_start")
         x_end_coordinate = request.form.get("x_end")
         y_end_coordinate = request.form.get("y_end")
+        successful = "successful" in request.form
+
+        #testing that values are returned from the form correctly
         print("Action:", action_type, "X:", x_start_coordinate, "Y:", y_start_coordinate)
         print("Action:", action_type, "X:", x_end_coordinate, "Y:", y_end_coordinate)
-    
-    
+        print(successful)
+
 
     #temporary variables 
-    #player = "Bukayo Saka"
     successful = True
     shot_x_coordinate = "0.85"
     shot_y_coordinate = "0.25"
 
     return render_template('index.html', type=action_type,
-                            #player=player,
                             x_start_coordinate=x_start_coordinate,
                             y_start_coordinate=y_start_coordinate,
                             x_end_coordinate=x_end_coordinate,
                             y_end_coordinate=y_end_coordinate,
                             successful=successful,
-                            shot_x_coordinate=shot_x_coordinate,
-                            shot_y_coordinate=shot_y_coordinate)
+                            )
 
 if __name__ == '__main__':
     app.run(debug=True)
