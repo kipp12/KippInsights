4d261bf4c545c5bd794e151e3e26fe28c606281b
kipp <batchelork3@cardiff.ac.ukgit config --global user.email batchelork3@cardiff.ac.uk>
2025-08-26 23:06:50 +0100

    inputted data is now stored in a list, multiple actions can be stored.

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import pandas as pd
+
+events = []
+count = 0
 
 app = Flask(__name__)
 
+def create_empty_dataframe():
+    df = pd.DataFrame(columns=["action_id", "sequence_id", "action_type","successful", "x_start_coord", "y_start_coord","x_end_coord", "y_end_coord"])
+    return df
 
+
+    
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     
+    
+    successful = False
     action_type = None
     x_start_coordinate = None
     y_start_coordinate = None
@@ -28,11 +38,30 @@ def index():
         print("Action:", action_type, "X:", x_end_coordinate, "Y:", y_end_coordinate)
         print(successful)
 
+        # Each time a new event happens:
+        new_event = {
+            "action_id": len(events)+1,
+            "sequence_id": 1,
+            "action_type": action_type,
+            "successful": successful,
+            "x_start_coord": x_start_coordinate,
+            "y_start_coord": y_start_coordinate,
+            "x_end_coord": x_end_coordinate,
+            "y_end_coord": y_end_coordinate
+        }
+        events.append(new_event)
 
-    #temporary variables 
-    successful = True
-    shot_x_coordinate = "0.85"
-    shot_y_coordinate = "0.25"
+        print()
+        print("event added:")
+        print(new_event)
+        # Redirect to GET page
+        return redirect(url_for("index"))
+
+    
+    
+    print()
+    print("all events:")
+    print(events)
 
     return render_template('index.html', type=action_type,
                             x_start_coordinate=x_start_coordinate,
